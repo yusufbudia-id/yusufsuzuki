d1c6383df38c2653ed241e29ecd4c8f8d7143235
Yusuf <abdullahyusufb@gmail.com>
2026-04-15 15:21:52 +0700

    Extract web.zip and add source code

--- app/promo/page.tsx
+++ app/promo/page.tsx
@@ -43,7 +43,7 @@ export default function PromoPage() {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10">
           {promos.map((promo, i) => (
-            <PromoCard key={promo.id} promo={promo} index={i} />
+            <PromoCard key={promo.slug} promo={promo} index={i} />
           ))}
         </div>
       </div>
