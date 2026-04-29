2016b1977214e0376ddb43162f8c0addbbf5917f
Yusuf <abdullahyusufb@gmail.com>
2026-04-29 14:38:56 +0700

    Extract web.zip and add source code

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
 import Hero from "@/components/Hero";
-// import FeaturedCars from "@/components/FeaturedCars"; // Bisa dihapus
+import FeaturedCars from "@/components/FeaturedCars";
 import AdvantagesSection from "@/components/AdvantagesSection";
 import PromoSection from "@/components/PromoSection";
 import TestimonialSection from "@/components/TestimonialSection";
 import ContactCTA from "@/components/ContactCTA";
 import MapSection from "@/components/MapSection";
 
+// Metadata di sini dihapus agar mengikuti layout.tsx secara otomatis
+
 export default function HomePage() {
   return (
     <>
       <Hero />
-      {/* <FeaturedCars /> dihapus karena sudah masuk ke Hero */}
+      <FeaturedCars />
       
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <hr className="border-t border-gray-200/10" /> {/* Ganti warna hr agar cocok kalau background bawahnya juga dark */}
+        <hr className="border-t border-gray-200" />
       </div>
       
       <PromoSection />
