4b864b8454dfe7c2600305e27eb054bc7e7c08d6
Yusuf <abdullahyusufb@gmail.com>
2026-05-16 22:05:58 +0700

    Extract web.zip and add source code

--- next.config.js
+++ next.config.js
@@ -6,6 +6,17 @@ const nextConfig = {
       { protocol: "https", hostname: "i.pravatar.cc" },
     ],
   },
+  
+  // Menambahkan aturan pengalihan (Redirect 301) untuk SEO
+  async redirects() {
+    return [
+      {
+        source: '/promo/promo-santai-suzuki-fronx-juli', // URL lama yang terlanjur di-index Google
+        destination: '/promo/promo-santai-suzuki-fronx-jogja', // URL baru yang benar
+        permanent: true, // true = 301 Redirect (Pindah Permanen)
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
